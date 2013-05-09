821264cd60dd6abc09c70c612f5aeb28419a4373
Jeroen Engels <jfm.engels@gmail.com>
2013-05-08 21:56:09 -0400

    Commented the code.

--- asserter.py
+++ asserter.py
@@ -1,60 +1,129 @@
 
 
 class Asserter:
+	"""Object which will assert statements, and store their result.
+"""
+
 	def __init__(self):
+		"""Initializes the asserter.
+"""
 		self.reset()
 
+
 	def reset(self):
-		self.val = True
-		self.resFunc = {}
-		self.resMsg = {}
-		self.currentFunction = None
+		"""Resets the internal data of the asserter so that new tests can be realized.
+"""
+		# End result: True if all tests succeeded, False otherwise.
+		self.successFinal = True
+		# Dictionary of the success result for each function.
+		self.successFunc = {} # key: function name, value: success boolean
+		# Dictionary of the fail messages for each function
+		self.messageFunc = {} # key: function name, value: list of fail messages
+		# Name of the function that is currently tested.
+		self.currentFunction = ''
+
 
 	def enter(self, func):
+		"""Enter a new function. Should be called every time a new test
+function is entered, otherwise failed functions will not be correctly shown.
+func: Name of the function to enter and to prepare for.
+"""
 		self.currentFunction = func
-		self.resFunc[func] = True
-		self.resMsg[func] = []
+		self.successFunc[func] = True
+		self.messageFunc[func] = []
 
-	def notifyFail(self, msg=''):
-		self.resFunc[self.currentFunction] = False
-		self.val = False
+
+	def _notifyFail(self, msg=''):
+		"""Notify the asserter of a failed statement.
+"""
+		# Fail the whole test and the current function.
+		self.successFinal = False
+		self.successFunc[self.currentFunction] = False
+		# If there is a message, append it.
 		if msg != '':
-			self.resMsg[self.currentFunction].append(msg)
+			self.messageFunc[self.currentFunction].append(msg)
 
-	def assertTrue(self, b, msg=''):
-		if not b:
-			self.notifyFail(msg)
 
-	def assertFalse(self, b, msg=''):
-		if b:
-			self.notifyFail(msg)
+	def catchException(self, exception, msg=''):
+		"""Catches an exception.
+exception: Catched exception.
+msg: Optional message to display. Will precede the exception's message.
+"""
+		if msg != '':
+			msg = msg + '\n' + repr(exception)
+		else:
+			msg = repr(exception)
+		self._notifyFail(msg)
+		
+
+	def assertTrue(self, value, msg=''):
+		"""Asserts that the given expression is evaluated as True.
+value: Value to evaluate.
+msg: Optional message to display if the assertion fails.
+"""
+		if not value:
+			self._notifyFail(msg)
+
+
+	def assertFalse(self, value, msg=''):
+		"""Asserts that the given expression is evaluated as False.
+value: Value to evaluate.
+msg: Optional message to display if the assertion fails.
+"""
+		if value:
+			self._notifyFail(msg)
+
 
 	def fail(self, msg=''):
-		self.notifyFail(msg)
+		"""Fails the test without condition.
+msg: Optional message to display.
+"""
+		self._notifyFail(msg)
+
 
 	def assertEquals(self, a, b, msg=''):
+		"""Asserts that a equals b.
+a, b: Values to evaluate.
+msg: Optional message to display if the assertion fails.
+"""
 		if a != b:
-			self.notifyFail(msg)
+			self._notifyFail(msg)
 
-	def assertNotNone(self, val, msg=''):
-		if val == None:
-			self.notifyFail(msg)
 
-	def assertNone(self, val, msg=''):
-		if val != None:
-			self.notifyFail(msg)
+	def assertNotNone(self, value, msg=''):
+		"""Asserts that value is not None.
+value: Value to evaluate.
+msg: Optional message to display if the assertion fails.
+"""
+		if value == None:
+			self._notifyFail(msg)
+
+
+	def assertNone(self, value, msg=''):
+		"""Asserts that value is None.
+value: Value to evaluate.
+msg: Optional message to display if the assertion fails.
+"""
+		if value != None:
+			self._notifyFail(msg)
+
 
 	def __repr__(self):
+		"""Represents the result of the test.
+"""
 		s = ''
-		if self.val:
+		if self.successFinal:
 			s = '[ OK ] Test'
 		else:
+			# Tests failed, display the results of each function.
 			s = '[FAIL] Test'
-			for func in self.resFunc:
+			for func in self.successFunc: # for every test function
 				s += '\n'
-				if not self.resFunc[func]:
+				if not self.successFunc[func]:
+					# If test function failed, print error
 					s += '[FAIL] ' + func
-					for msg in self.resMsg[func]:
+					# and error messages.
+					for msg in self.messageFunc[func]:
 						s += '\n\t' + msg
 				else:
 					s += '[ OK ] ' + func
--- testCase.py
+++ testCase.py
@@ -1,32 +1,51 @@
 from asserter import Asserter
 
 class TestCase:
-	def __init__(self):
-		pass
+	"""Model for a unit test.
+"""
 
 	def run(self):
-		self.asserter = Asserter()
-		self.setUpBeforeClass()
+		"""Run the tests. Executes every function whose name starts
+with 'test'.
+"""
+		if self.asserter == None:
+			self.asserter = Asserter()
+		self.asserter.reset()
 		functions = [func for func in dir(self) \
 			if func.startswith('test')]
+		self.setUpBeforeClass() # One-time global set up.
+
+		# Execute every function.
 		for func in functions:
+			# Notify the asserter a new function is beginning.
 			self.asserter.enter(func)
 			self.setUp()
-			getattr(self, func)()
+			try:
+				getattr(self, func)() # Launch function
+			except Exception as exc: # Catch uncaught exceptions
+				self.asserter.catchException(exc)
 			self.tearDown()
-		self.tearDownAfterClass()
-		print('Test result:', self.asserter)
+		self.tearDownAfterClass() # Final tear down
+		print('Test result:', self.asserter) # Print result
 		
 
 	def setUpBeforeClass(self):
+		"""One-time set up before all the test functions are launched.
+"""
 		pass
 
 	def tearDownAfterClass(self):
+		"""One-time tear down, called when all the tests have been realized.
+"""
 		pass
 
 	def setUp(self):
+		"""Set up ressources. Is called before every function launch.
+"""
 		pass
 
 	def tearDown(self):
+		"""Tear down ressources. Is called after every function launch.
+"""
 		pass
 
